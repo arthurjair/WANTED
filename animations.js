9d5823013e263d7225d7fe142435cc8b0a4a7beb
arthu <i532632.fhict.nl>
2024-05-16 22:18:32 +0200

    new portfolio

--- animations.js
+++ animations.js
@@ -1,44 +1,5 @@
-var coll = document.getElementsByClassName("collapsible");
-            var i;
-            
-            for (i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.maxHeight) {
-                  content.style.maxHeight = null;
-                } else {
-                  content.style.maxHeight = content.scrollHeight + "px";
-                }
-              });
-            }
-var coll = document.getElementsByClassName("collapsible2");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".grayscale");
-    const threshold = 0; // Adjust this value to control when the transition happens
-  
-    function isElementInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      return (
-        rect.top >= -window.innerHeight * (1 - threshold) &&
-        rect.bottom <= (window.innerHeight * threshold || document.documentElement.clientHeight)
-      );
-    }
-  
-    function handleScroll() {
+ function handleScroll() {
       images.forEach(image => {
         const isVisible = isElementInViewport(image);
         if (isVisible) {
@@ -51,4 +12,3 @@ document.addEventListener("DOMContentLoaded", function() {
   
     window.addEventListener("scroll", handleScroll);
     handleScroll(); // Initial check on page load
-  });
