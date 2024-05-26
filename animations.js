007ec15b5556102e74a05cac72714fc5374e8ed1
arthu <i532632.fhict.nl>
2024-05-26 14:59:42 +0200

    working on second container

--- animations.js
+++ animations.js
@@ -10,8 +10,6 @@ function handleScroll() {
       });
 }
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const leftDiv = document.querySelector('.left');
     const rightDiv = document.querySelector('.right');
@@ -25,11 +23,30 @@ document.addEventListener("DOMContentLoaded", function() {
         const leftDivHeight = leftDiv.offsetHeight;
 
         if (scrollTop + initialLeftTop + leftDivHeight > containerBottom) {
-            leftDiv.style.top = (containerBottom - leftDivHeight - initialLeftTop) + 'px';
+            leftDiv.style.top = (containerBottom - leftDivHeight - initialLeftTop) + '0px';
         } else {
-            leftDiv.style.top = '0px';
+            leftDiv.style.top = '130px';
         }
     });
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const leftDiv = document.querySelector('.left2');
+  const rightDiv = document.querySelector('.right2');
+  const containerContent = document.querySelector('.content');
+  const initialLeftTop = leftDiv.getBoundingClientRect().top;
+
+  window.addEventListener('scroll', function() {
+      const scrollTop = window.scrollY;
+      const rightDivBottom = rightDiv.getBoundingClientRect().bottom + window.scrollY;
+      const containerBottom = containerContent.getBoundingClientRect().bottom + window.scrollY;
+      const leftDivHeight = leftDiv.offsetHeight;
+
+      if (scrollTop + initialLeftTop + leftDivHeight > containerBottom) {
+          leftDiv.style.top = (containerBottom - leftDivHeight - initialLeftTop) + 'px';
+      } else {
+          leftDiv.style.top = '0px';
+      }
+  });
 });
 
 
